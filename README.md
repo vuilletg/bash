06c192d87de2fa087b13aaa554ea07d9a2243f3d
vuilletg <gaspard.vuillet@gmail.com>
2026-02-09 17:21:53 +0100

    ajout fonctionalité bonus(paneau,cr,cd)

--- README.md
+++ README.md
@@ -1,32 +1,33 @@
 # Compte rendu TP Mini-Shell 
 
 ## Auteur
-- **Gaspard Vuillet**
+**Gaspard Vuillet**
 
 ## Fonctionnalités implémentées
 ### Fonctionnalités de base
-- [x] **Exécution de programmes** : Lancement de commandes simples.
-- [x] **Redirections** : Gestion des entrées/sorties avec `<` et `>` .
-- [x] **Gestion des erreurs** : Affichage de messages explicites sur la sortie d'erreur.
-- [x] **Tubes (Pipes)** : Gestion d'une séquence de deux commandes reliées par `|`.
+- **Exécution de programmes** : Lancement de commandes simples.
+- **Redirections** : Gestion des entrées/sorties avec `<` et `>` .
+- **Gestion des erreurs** : Affichage de messages explicites sur la sortie d'erreur.
+- **Tubes (Pipes)** : Gestion d'une séquence de deux commandes reliées par `|`.
 
 ### Fonctionnalités avancées
-- [x] **Arrière-plan (`&`)** : Exécution de tâches de fond sans attendre leur terminaison via la fonction `test_arriere_plan()`.
-- [x] **Nettoyage des zombies** : Utilisation du signal `SIGCHLD` pour ramasser les processus fils terminés en arrière-plan.
-- [x] **Signaux** : Gestion du `Ctrl+C` pour ne pas interrompre le shell lui-même.
-
+- **Arrière-plan (`&`)** : Exécution de tâches de fond sans attendre leur terminaison via la fonction `test_arriere_plan()`.
+- **Nettoyage des zombies** : Utilisation du signal `SIGCHLD` pour ramasser les processus fils terminés en arrière-plan.
+- **Signaux** : Gestion du `Ctrl+C` pour ne pas interrompre le shell lui-même.
+- **Commande** : implémentation de la commande `cd` avec gestion des erreurs et affichage du dossier courant.
+cd
 ## Bugs connus
-- **commande**: certaines commandes indisponibles: ex cd
 - **Entrée/Sortie** : Le message `fgets: No child processes` s'affiche lors d'une sortie normale du shell si une commande a été exécutée auparavant.
 - **Signaux** : Le `Ctrl+Z` suspend l'intégralité du shell au lieu de suspendre uniquement le processus en cours d'exécution.
 
 ## Instructions de compilation
-Le code source est conçu pour être compilé sur les machines de l'UFR.
 1. ### Méthode 1:
 - dans un terminal executez :```./reload_shell``` (script de compilation du code et execution)
 2. ### Méthode 2: 
 - executez: `make`
 - executez: `./shell`
 
-### Bonus
-le programe `loop.c` est une boucle infini (peut être utiliser pour verifier le comportement du shell)
+## Bonus
+- le programe `loop.c` est une boucle infini (peut être utiliser pour verifier le comportement du shell) 
+
+**!!compilation à effectuer separément car hors makefile!!**
--- shell.c
+++ shell.c
@@ -7,36 +7,40 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include "shell-utils.h"
 
 #define INPUT_BUFFER_SIZE 2048
+#define CURRENT_DIRECTORY_SIZE 2048
 #define MAX_TOKENS 256
-void wait_handler(int sig){
-    waitpid(-1, NULL,WNOHANG);
+char CD[CURRENT_DIRECTORY_SIZE];
+void wait_handler(int sig) {
+    waitpid(-1, NULL, WNOHANG);
 }
+
 /**
  *  fonction perméttant d'executer une commande avec ou sans redirection
- * @param tokens le programe à executer avec ses paramètres
+ * @param tokens le programe à executer à l'indice 0 avec ses paramètres
  */
-void lancementCommande(char** tokens){
-    char* red=trouve_redirection(tokens,"<");
-    if(red != NULL){
+void lancementCommande(char **tokens) {
+    char *red = trouve_redirection(tokens, "<");
+    if (red != NULL) {
         int fd = open(red, O_RDONLY);
         if (fd == -1) {
-            fprintf(stderr, "%s: Impossible d'ouvrir le fichier '%s': %s\n",tokens[0], red, strerror(errno));
+            fprintf(stderr, "%s: Impossible d'ouvrir le fichier '%s': %s\n", tokens[0], red, strerror(errno));
             exit(1);
         }
-        dup2(fd,0);
+        dup2(fd, 0);
         close(fd);
-    } else{
-        red=trouve_redirection(tokens,">");
-        if(red != NULL){
+    } else {
+        red = trouve_redirection(tokens, ">");
+        if (red != NULL) {
             int fd = open(red, O_WRONLY | O_CREAT | O_TRUNC, 0644);
             if (fd == -1) {
-                fprintf(stderr, "%s: Impossible d'ouvrir le fichier '%s': %s\n",tokens[0], red, strerror(errno));
+                fprintf(stderr, "%s: Impossible d'ouvrir le fichier '%s': %s\n", tokens[0], red, strerror(errno));
                 exit(1);
             }
-            dup2(fd,1);
+            dup2(fd, 1);
             close(fd);
         }
     }
@@ -49,12 +53,13 @@ void lancementCommande(char** tokens){
     fprintf(stderr, "Commande introuvable: %s\n", tokens[0]);
     exit(1);
 }
+
 int main() {
     /* Une variable pour sotcker les caractères tapés au clavier */
     char line[INPUT_BUFFER_SIZE + 1];
-    signal(SIGINT,SIG_IGN);
+    signal(SIGINT, SIG_IGN);
     signal(SIGTSTP, SIG_IGN);
-    signal(SIGCHLD,wait_handler);
+    signal(SIGCHLD, wait_handler);
     /* Une variable qui pointera vers les données lues par fgets
      * Sauf problème (erreur, plus de données, ...), elle pointera en
      * fait vers la variable précédente (line) */
@@ -67,8 +72,18 @@ int main() {
     char *tokens[MAX_TOKENS + 1];
     /* variables entières pour compter le nombre de token */
     int nb_tokens;
+    printf("\n   █████████  █████   █████ ██████████ █████       █████      \n"
+           "  ███▒▒▒▒▒███▒▒███   ▒▒███ ▒▒███▒▒▒▒▒█▒▒███       ▒▒███       \n"
+           " ███     ▒▒▒  ▒███    ▒███  ▒███  █ ▒  ▒███        ▒███       \n"
+           "▒███          ▒███████████  ▒██████    ▒███        ▒███       \n"
+           "▒███    █████ ▒███▒▒▒▒▒███  ▒███▒▒█    ▒███        ▒███       \n"
+           "▒▒███  ▒▒███  ▒███    ▒███  ▒███ ▒   █ ▒███      █ ▒███      █\n"
+           " ▒▒█████████  █████   █████ ██████████ ███████████ ███████████\n"
+           "  ▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒   ▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒▒ \n"
+           "                                                              \n");
     while (1) {
-        printf("commande:");
+        getcwd( CD, CURRENT_DIRECTORY_SIZE );
+        printf("%s:",CD);
         fflush(stdout);
         /* Récupération des données entrées au clavier */
         data = fgets(line, INPUT_BUFFER_SIZE, stdin);
@@ -112,40 +127,54 @@ int main() {
                     if (nb_tokens <= 0) {
                         fprintf(stderr, "No tokens found: exiting\n");
                     } else {
-                        if(strcmp(tokens[0],"exit")==0){ exit(0);}
-                        int ap =0;
-                        ap = test_arriere_plan(tokens,"&");
-                        pid_t pid = fork();
-                        if (pid == 0) {
-                            signal(SIGINT, SIG_DFL);
-                            signal(SIGTSTP, SIG_DFL);
-                            char **sec;
-                            if ((sec = trouve_tube(tokens,"|"))!=NULL){
-                                int fd[2];
-                                if(pipe(fd)==-1){
-                                    perror("pipe");
-
-                                    exit(1);
+                        if (strcmp(tokens[0], "exit") == 0) { printf("Au revoir \n"); exit(0); }
+                        if (strcmp(tokens[0],"cd")==0){
+                            if(tokens[1]!=NULL){
+                                if(chdir(tokens[1])!=0){
+                                    fprintf(stderr, "cd: imposible d'aller au repertoire %s:%s\n",tokens[1], strerror(errno));
                                 }
-                                if(fork()==0){
-                                    close(fd[0]);
-                                    dup2(fd[1],1);
-                                    lancementCommande(tokens);
-                                }else{
-                                    wait(NULL);
-                                    close(fd[1]);
-                                    dup2(fd[0],0);
-                                    lancementCommande(sec);
-                                }
-                            }else{
-                                lancementCommande(tokens);
+                            } else{
+                                chdir(getenv("HOME"));
                             }
-                        } else{
+                        }
+                        else {
+                            int ap = 0;
+                            ap = test_arriere_plan(tokens, "&");
+                            pid_t pid = fork();
+                            if (pid == 0) {
+                                signal(SIGINT, SIG_DFL);
+                                signal(SIGTSTP, SIG_DFL);
+                                char **sec;
+                                if (strcmp(tokens[0], "copyright") == 0) {
+                                    printf("Auteur: Gaspard Vuillet :)\n");
+                                    exit(0);
+                                } else if ((sec = trouve_tube(tokens, "|")) != NULL) {
+                                    int fd[2];
+                                    if (pipe(fd) == -1) {
+                                        fprintf(stderr, "%s: création de tube imposible: %s\n", tokens[0],
+                                                strerror(errno));
+                                        exit(1);
+                                    }
+                                    if (fork() == 0) {
+                                        close(fd[0]);
+                                        dup2(fd[1], 1);
+                                        lancementCommande(tokens);
+                                    } else {
+                                        wait(NULL);
+                                        close(fd[1]);
+                                        dup2(fd[0], 0);
+                                        lancementCommande(sec);
+                                    }
+                                } else {
+                                    lancementCommande(tokens);
+                                }
+                            } else {
 
-                            if(ap==1) {
-                                printf("[PID: %d]\n", pid);
-                            }else{
-                                waitpid(-1, NULL,  0);
+                                if (ap == 1) {
+                                    printf("[PID: %d]\n", pid);
+                                } else {
+                                    waitpid(-1, NULL, 0);
+                                }
                             }
                         }
                     }
